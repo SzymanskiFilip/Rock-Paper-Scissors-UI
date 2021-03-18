dfdb87e0afa6abcadc3b0c1ca1cb8a77dea17783
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-18 19:48:43 +0100

    added a computerPlay function that returns the random option from the options array

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const buttons = document.querySelectorAll('.button');
+const options = ["Rock", "Paper", "Scissors"];
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         alert(button.id);
     });
 });
+
+function computerPlay(){
+    const random = options[Math.floor(Math.random() * options.length)];
+    return random;
+}
