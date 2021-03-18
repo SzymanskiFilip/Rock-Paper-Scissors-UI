4ce2a7ffc60292f44035fda64dd2a3fd18af30b9
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-18 21:12:41 +0100

    connected the variables with the HTML id's

---
+++ script.js
@@ -0,0 +1,9 @@
+const userScore = 0;
+const computerScore = 0;
+
+const userScoreResult = document.getElementById('user-score');
+const computerScoreResult = document.getElementById('computer-score');
+
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
