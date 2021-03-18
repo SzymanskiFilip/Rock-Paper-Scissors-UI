76e38762e1b77f2c21694ff8926a61366af2c4f5
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-18 21:24:35 +0100

    created a getComputerChoice func that randomizes the choice

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
 
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+}
+
 
 function game(userChoice){
-    console.log(userChoice);
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice);
 }
 
 function main(){
