40c7dbdb2b0c799ad501e4f000460ad13c3fa9a4
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-18 20:08:19 +0100

    deleted previous changes, added a function that returns the clicked id which got passed from html element

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const buttons = document.querySelectorAll('.button');
 const options = ["Rock", "Paper", "Scissors"];
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        alert(button.id);
-    });
-});
-
+//computer picks random option
 function computerPlay(){
     const random = options[Math.floor(Math.random() * options.length)];
     return random;
+}
+
+
+//returns the id of the clicked button
+function playersOption(clickedId){
+    return clickedId;
 }
